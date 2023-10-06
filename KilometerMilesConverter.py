59b904d66c360bdb7f7829798222025da2564a6b
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2023-10-06 13:36:59 +0530

    Update KilometerMilesConverter.py

--- KilometerMilesConverter.py
+++ KilometerMilesConverter.py
@@ -1,9 +1,12 @@
+#Function to convert kilometer to miles
 def kilometer_to_miles(value):
     return value * 0.6214
 
+#Function to convert miles to kilometer
 def miles_to_kilometer(value):
     return value / 0.6214
 
+#This loop will continue until we exit from the program
 while True:
     try:
         print("Select the option below: ")
@@ -15,12 +18,12 @@ while True:
         
         if value==1:
             km = float(input("Enter the value in km: "))
-            mile = round(kilometer_to_miles(km),2)
+            mile = round(kilometer_to_miles(km),2) #round off the value to 2 precision places
             print(f"{km} km is equal to {mile} miles")
 
         elif value ==2:
             mile = float(input("Enter the value in miles: "))
-            km = round(miles_to_kilometer(mile),2)
+            km = round(miles_to_kilometer(mile),2)  #round off the value to 2 precision places
             print(f"{mile} miles is equal to {km} km")
 
         elif value ==3:
