272a63fe14fe72d6cd90e2616d2522cadb0f0ffa
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2023-10-06 13:33:47 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,4 +1,4 @@
-
+#Taking the input from the user
 num = int(input("Enter a number: "))
 
 #Checking if number is a positive number
@@ -7,9 +7,10 @@ if(num>0):
     factorial = 1
     i = 1
 
+    #this loop will continue till i is less than or equal to the number
     while i<=num:
-        factorial = factorial * i
-        i += 1
+        factorial *= i #factorial = factorial * i
+        i += 1 #incrementing the value of i by 1
 
     print(f"Factorial of {original_num}:",factorial)
     
