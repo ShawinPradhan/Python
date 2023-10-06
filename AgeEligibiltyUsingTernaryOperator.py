5bf85fc15885b401f4f5d3a4a13527bc304ce139
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2023-10-06 13:21:04 +0530

    Update AgeEligibiltyUsingTernaryOperator.py

--- AgeEligibiltyUsingTernaryOperator.py
+++ AgeEligibiltyUsingTernaryOperator.py
@@ -1,15 +1,19 @@
-
+#function to check whether a person is eligible to vote or not
 def check_eligibility(age):
     eligible = "Eligible to vote" if age>=18 else "Not eligible to vote"
     return eligible;
 
 try:
+    #Taking age as input fro the user
     age = int(input("Enter your age to check for voting eligibility: "))
     result = check_eligibility(age)
     print(result)
+#handling the ValueError exception
 except ValueError:
     print("Please enter a valid age.")
 
-
+# OUTPUT
+# Enter your age to check for voting eligibility: 18
+# Eligible to vote
 
 
