7e09f3b9a1eaf3204a10c6a5370c18051681f452
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2023-10-06 13:22:04 +0530

    Update AreaOfTriangle.py

--- AreaOfTriangle.py
+++ AreaOfTriangle.py
@@ -1,9 +1,12 @@
+#function to calculate the area of triangle
 def area_of_triangle(base,height):
     return 0.5 * base * height;
 
+#Taking the base and height as input from the user
 base = float(input("Enter the base of the triangle: "))
 height = float(input("Enter the height of the triangle: "))
 
+#Printing the area of triangle
 print(f"Area of triangle= ", area_of_triangle(base,height))
 
 ##OUTPUT
