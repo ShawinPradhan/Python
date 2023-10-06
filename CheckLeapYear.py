4a04c0b75095d7ea25a02b3fa4b8cae94f59ff2c
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2023-10-06 13:29:06 +0530

    Update CheckLeapYear.py

--- CheckLeapYear.py
+++ CheckLeapYear.py
@@ -1,4 +1,4 @@
-
+#function to check whether a year is a leap year or not
 def check_leap_year(year):
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return " is a leap year."
