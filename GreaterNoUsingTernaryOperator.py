e009d5ecca52432ae9dc09fcc3b10ab0f4b116a9
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2023-10-06 13:35:09 +0530

    Update GreaterNoUsingTernaryOperator.py

--- GreaterNoUsingTernaryOperator.py
+++ GreaterNoUsingTernaryOperator.py
@@ -1,10 +1,12 @@
+#function to check which of the number is the greater no using ternary operator
 def greaterNo(num1,num2):
     return num1 if num1>num2 else num2
 
-
+#Taking inout from the user
 num1 = float(input("Enter the first digit: "))
 num2 = float(input("Enter the second digit: "))
 
+#Printing the greater number
 print("Greater number is: ",greaterNo(num1,num2))
 
 ##OUTPUT
