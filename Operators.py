82eb902a18ae5e9620ed59533aee12b782b40dbb
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2023-11-01 12:55:47 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,73 @@
+#Arithmetic Operators
+num1 = 25
+num2 = 13
+addition = num1 + num2
+division = num1 / num2
+modulus = num1 % num2
+power = 5**2
+floor = num1//num2
+print(addition)
+print(division)
+print(modulus)
+print(power)
+print(floor)
+
+print()
+#Comparision Operators
+# == (equal to) , != (not equal to), <(less than), >(greater than)
+# <= (less than or equal to) , >= (greater than or equal to)
+print(num1>num2)
+print(num1==num2)
+print(num1!=num2)
+
+print()
+#Logical Operators
+# and (Logical AND), or (Logical OR), not (Logical Not)
+is_true = True
+is_false = False
+print(is_true and is_false)
+print(is_true or is_false)
+print(not is_true)
+
+print()
+#Assignment Operators
+# = (Assignment)
+# Augmented Assignment -> += , -=, *=, **=, //=, /=
+num3 = 5
+num3 *= 3 # num = num * 3
+print(num3) #15
+num3 **= 2 # num = num**2
+print(num3)
+
+num4 = 5
+num4 //= 2 
+print(num4)
+
+#Ternary Operator (conditional expression)
+#syntax -> value_if_true if condition else value_if_false
+age = 17
+eligibility = "Eligible to vote" if age>=18 else "Not eligible to vote"
+print(eligibility)
+
+result = (15 + 23) * 9
+print(result)
+num1 = 5
+num2 = 10
+num3 = 15
+print(num1<num2<=num3)
+
+value = 8
+value += 2 * 3
+print(value)
+
+print(2*(3+4)-5**2)
+
+
+
+
+
+
+
+
+
+
---
+++ VariableDeclaration.py
@@ -0,0 +1,39 @@
+import keyword as ky
+
+reserved_keywords = ky.kwlist
+print(reserved_keywords)
+
+#Variable declaration and assignment
+total = 23
+name = "John"
+
+#Update the varable values
+total = 50
+
+print(total)
+print(name)
+
+print(type(total))
+print(type(name))
+
+#Constant declaration (using uppercase)
+PI = 3.14
+
+print(PI)
+print(type(PI))
+
+char = 'c'
+print(type(char))
+
+num1 = 20
+num2 = 25
+
+if num2>num1:
+    print("num2 is greater than num1")
+
+#snake_case => my_name_is_shawin
+#CamelCase => myNameIsShawin
+
+is_valid = True
+
+
